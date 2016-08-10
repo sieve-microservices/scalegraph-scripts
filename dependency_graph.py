7c0c60cd5db67902b07ec437d813db6325c40312
Jörg Thalheim <joerg@higgsboson.tk>
2016-08-10 09:13:34 +0200

    update code to draw dependency graph

--- dependency_graph.py
+++ dependency_graph.py
@@ -1,12 +1,11 @@
 import sys
-import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-EXCLUDE=["mongodb", "redis", "haproxy", "postgresql"]
+EXCLUDE=[]
 
-def write_dot(dot_file, paths, significance=0.05, include_insignificant=False):
+def write_dot(dot_file, paths, significance=0.01, include_insignificant=False):
     dot_file.write("""
 digraph {
   overlap = false
@@ -15,6 +14,8 @@ digraph {
 
     for path in paths:
         df = pd.read_csv(path, sep="\t")
+        if len(df) == 0:
+            continue
         # bug in causation!
         df.drop_duplicates(subset=["perpetrator", "consequence"], inplace=True)
         rows = df.iterrows()
@@ -29,52 +30,27 @@ digraph {
             if service_a in EXCLUDE or service_b in EXCLUDE:
                 continue
             color = "black"
-            if row_a.p_for_lag_1 < significance and row_b.p_for_lag_1 < significance:
+            if row_a.p_for_lag_1 <= significance and row_b.p_for_lag_1 <= significance:
+                continue
                 dir = "both"
                 symbol = "<->"
-            elif row_a.p_for_lag_1 < significance:
+            elif row_a.p_for_lag_1 <= significance:
                 dir = "forward"
                 symbol = "->"
-            elif row_b.p_for_lag_1 < significance:
+            elif row_b.p_for_lag_1 <= significance:
                 dir = "back"
                 symbol = "<-"
             elif include_insignificant:
                 dir = "both"
                 symbol = "<->"
                 color = "red"
+            else:
+                continue
             weight = 1 - min(row_a.p_for_lag_1, row_b.p_for_lag_1)
             args = (service_a, service_b, row_a.perpetrator, symbol, row_a.consequence, weight, weight, dir, color)
             dot_file.write('  "%s" -> "%s" [tooltip="%s %s %s (%f)",penwidth=%.3f,dir=%s,color=%s]\n' % args)
 
     dot_file.write("}")
-
-def build_graph(gexf_path, dot_path, paths, significance=0.05):
-    g = nx.MultiDiGraph()
-    for path in paths:
-        df = pd.read_csv(path, sep="\t")
-        # bug in causation!
-        df.drop_duplicates(subset=["perpetrator", "consequence"], inplace=True)
-        rows = df.iterrows()
-        while True:
-            try:
-                row_a = next(rows)[1]
-                row_b = next(rows)[1]
-            except StopIteration:
-                break
-            service_a, metric_a = row_a.perpetrator.split("-", 1)
-            service_b, metric_b = row_a.consequence.split("-", 1)
-            if service_a in EXCLUDE or service_b in EXCLUDE:
-                continue
-
-            if row_a.p_for_lag_1 < significance:
-                weight = (1-row_a.p_for_lag_1) * 10 - 9
-                g.add_edge(service_a, service_b, name="%s -> %s" % (metric_a, metric_b), weight=weight)
-            if row_b.p_for_lag_1 < significance:
-                weight = (1-row_b.p_for_lag_1) * 10 - 9
-                g.add_edge(service_b, service_a, name="%s -> %s" % (metric_b, metric_a), weight=weight)
-
-    nx.draw(g, pos)
-    nx.write_gexf(g, gexf_path)
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
