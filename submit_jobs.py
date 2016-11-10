f7bed9c6337abfe0459740e12487dcdbc85112a1
Jörg Thalheim <joerg@higgsboson.tk>
2016-11-11 00:13:39 +0100

    submit_jobs: update

--- submit_jobs.py
+++ submit_jobs.py
@@ -8,18 +8,18 @@ import itertools
 import cluster
 
 from graphs import draw_graph
-from grangercausality import compare_services, load_graph
 import metadata
 
 from ipyparallel import Client
 rc = Client()
 lview = rc.load_balanced_view()
 
+def _cluster_service(args):
+    import cluster
+    return cluster.cluster_service(*args)
+
 def cluster_services(path):
     data = metadata.load(path)
-    def _cluster_service(args):
-        import cluster
-        return cluster.cluster_service(*args)
     ids = []
     for cluster_size in range(1, 8):
         for service in data["services"]:
@@ -35,30 +35,35 @@ def adfuller(paths):
     return lview.map(_draw, paths)
 
 def increase_cluster_size(path):
-    queue = Queue(connection=Redis("jobqueue.local"))
     data = metadata.load(path)
     best_score = -1
     best = -1
+    ids = []
     for service in data["services"]:
         for key, value in service.get("clusters", {}).items():
             score = value.get("silhouette_score", -1)
             if best_score < score:
                 best = int(key)
                 best_score = score
-        if best in [6, 7]:
+        if best_score < 0.5:
             for cluster_size in range(8, min(len(service["preprocessed_fields"]), 15)):
-                queue.enqueue_call(func=cluster_service, args=(path, service, cluster_size), timeout=3600*3)
+                print(path, service, cluster_size)
+                #res = lview.apply_async(_cluster_service, (path, service, cluster_size))
+                #ids.extend(res.msg_ids)
+    return ids
 
 def find_causality(callgraph_path, path):
     data = metadata.load(path)
     call_pairs = load_graph(callgraph_path)
+    call_pairs = undirected_callgraph(services_data["edges"])
     services = {}
     for srv in data["services"]:
         services[srv["name"]] = srv
     ids = []
     def _compare_services(args):
-        from grangercausality import compare_services
-        compare_services(*args)
+        import grangercausality
+        reload(grangercausality)
+        grangercausality.compare_services(*args)
     for srv_a, srv_b in call_pairs:
         res = lview.apply_async(_compare_services, (services[srv_a], services[srv_b], path))
         ids.extend(res.msg_ids)
@@ -80,7 +85,7 @@ if __name__ == "__main__":
     total = 0
     ids = []
     for arg in sys.argv[1:]:
-        res = find_causality("foo", arg)
+        res = find_causality(arg, "callgraph-sharelatex.json", arg)
         #res = increase_cluster_size(arg)
         #res = cluster_services(arg)
         ids.extend(res)
